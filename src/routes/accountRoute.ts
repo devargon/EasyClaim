d57ec0747e2731bb7ca8053b3a7e2115a37702aa
Argon <argon@nogra.xyz>
2024-07-11 17:43:18 +0800

    Add signup welcome page for user first visit

--- src/routes/accountRoute.ts
+++ src/routes/accountRoute.ts
@@ -2,17 +2,16 @@ import express, { Request, Response, NextFunction } from 'express';
 import {FormRegisterUser, LoginUser} from "../controllers/accountController";
 import {redirectIfLoggedIn} from "../middlewares/redirectIfLoggedIn";
 import {redirectAsRequiresLogin} from "../middlewares/redirectAsRequiresLogin";
-const router = express.Router();
+import prisma from "../config/db";
+import type {User} from "@prisma/client";
 
+const router = express.Router();
 /* GET home page. */
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.render('index', { title: 'Express' });
 });
 
 router.get('/login', redirectIfLoggedIn, (req: Request, res: Response, next: NextFunction) => {
-    if (req.user) {
-        return res.status(200).send("You're already logged in mofo");
-    }
     req.body.redirect = req.query.redirect || "/";
     res.render('login', { title: 'Login to EasyClaim', register_error: null, values: req.body });
 });
@@ -20,15 +19,23 @@ router.get('/login', redirectIfLoggedIn, (req: Request, res: Response, next: Nex
 router.post('/login', redirectIfLoggedIn, LoginUser)
 
 router.get('/signup', redirectIfLoggedIn, (req: Request, res: Response, next: NextFunction) => {
-    if (req.user) {
-        return res.status(200).send("You're already logged in mofo");
-    }
     req.body.redirect = req.query.redirect || "/";
     res.render('signup', { title: 'Sign up for EasyClaim', register_error: null, values: req.body});
 });
 
-router.get('/signup/success', redirectAsRequiresLogin, (req: Request, res: Response, next: NextFunction) => {
-    res.render('signupsuccessful', {title: "Signup Successful"})
+router.get('/signup/success', redirectAsRequiresLogin, async (req: Request, res: Response, next: NextFunction) => {
+    if (req.user) {
+        if (!req.user.hasSeenWelcomePage) {
+            await prisma.user.update({
+                where: {id: req.user.id},
+                data: {hasSeenWelcomePage: true}
+            });
+            return res.render('signupsuccessful', {title: "Signup Successful"})
+        }
+    }
+    return res.redirect("/");
+
+
 });
 
 router.post('/signup', redirectIfLoggedIn, FormRegisterUser);
