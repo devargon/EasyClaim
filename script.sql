d8a89bc822d554d83aceeeefebffc180b275d6ec
Argon <argon@nogra.xyz>
2024-07-13 16:28:07 +0800

    Add pug and prisma

---
+++ script.sql
@@ -0,0 +1,33 @@
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Utilities', 'lightning-charge');
+
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Transport', 'car-front');
+
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Food', 'basket');
+
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Health Care', 'capsule');
+
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Personal Care', 'brush');
+
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Education', 'book');
+
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Entertainment', 'controller');
+
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Gift', 'balloon');
+
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Home Improvement', 'tools');
+
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Pet Expenses', 'paw');
+
+INSERT INTO easyclaim.category (name, icon)
+VALUES ('Others', 'three-dots');
+
