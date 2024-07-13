34f98ab4324760b70f208938df6539c67bbbdf70
Argon <argon@nogra.xyz>
2024-07-13 16:29:04 +0800

    Improve loading spinner rendering

--- src/public/assets/javascripts/main.js
+++ src/public/assets/javascripts/main.js
@@ -1,33 +1,62 @@
 function makeButtonLoad (button) {
     button.setAttribute("disabled", "");
-    const spinner = button.querySelector(".spinner-border");
-    spinner.style.display = "inline-block";
+    const spinner = getSpinnerInButton(button);
+    if (spinner) {
+        spinner.style.display = "inline-block";
+    }
 }
 
 function makeButtonUnload (button) {
     button.removeAttribute("disabled");
-    const spinner = button.querySelector(".spinner-border");
-    spinner.style.display = "none";
+    const spinner = getSpinnerInButton(button);
+    if (spinner) {
+        spinner.style.display = "none";
+    }
 }
 
-// document.querySelectorAll(".spinner-on-click").forEach(button => {
-//     button.addEventListener("click", () => {
-//         makeButtonLoad(button);
-//     });
-// });
+function getSpinnerInButton(button) {
+    return button.querySelector(".spinner-border");
+}
+
+function findSubmitButtonInForm(form) {
+    return form.querySelector(`button[type="submit"]`);
+}
+
+function makeFormSubmitButtonUnload(form) {
+    const btn = findSubmitButtonInForm(form);
+    makeButtonUnload(btn);
+}
 
 if (document.forms.length > 0) {
     for (var i = 0; i < document.forms.length; i++) {
         const form = document.forms[i];
         console.log(form)
         form.addEventListener("submit", () => {
-            const submitBtn = form.querySelector(`button[type="submit"]`);
-            const buttonSpinner = submitBtn.querySelector(".spinner-border");
+            const submitBtn = findSubmitButtonInForm(form);
+            const buttonSpinner = getSpinnerInButton(submitBtn)
             if (buttonSpinner) {
                 makeButtonLoad(submitBtn);
             } else {
-                console.log("The submit button in this form doesn't have a spinner.")
+                console.log("The submit button in this form doesn't have a spinner.");
             }
         })
     }
+}
+
+function formToJSON(form) {
+    const data = new FormData(form);
+
+    let object = {};
+    data.forEach((value, key) => {
+        // Reflect.has in favor of: object.hasOwnProperty(key)
+        if(!Reflect.has(object, key)){
+            object[key] = value;
+            return;
+        }
+        if(!Array.isArray(object[key])){
+            object[key] = [object[key]];
+        }
+        object[key].push(value);
+    });
+    return JSON.stringify(object);
 }
