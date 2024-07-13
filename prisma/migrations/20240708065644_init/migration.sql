46684544a2d4061bb893fb673902b435e2960f8c
Argon <argon@nogra.xyz>
2024-07-13 16:28:35 +0800

    Fix migration

--- prisma/migrations/20240708065644_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `expense` ADD COLUMN `claimId` INTEGER NULL;
-
--- AddForeignKey
-ALTER TABLE `Expense` ADD CONSTRAINT `Expense_claimId_fkey` FOREIGN KEY (`claimId`) REFERENCES `Claim`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20240713075606_initial/migration.sql
+++ prisma/migrations/20240713075606_initial/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE `User` (
     `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `hasSeenWelcomePage` BOOLEAN NOT NULL DEFAULT false,
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
@@ -13,6 +14,7 @@ CREATE TABLE `User` (
 -- CreateTable
 CREATE TABLE `Claim` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
     `submittedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `status` ENUM('PENDING', 'COMPLETED') NOT NULL DEFAULT 'PENDING',
     `claimOffset` DECIMAL(10, 2) NULL,
@@ -32,12 +34,14 @@ CREATE TABLE `Category` (
 -- CreateTable
 CREATE TABLE `Expense` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `spentOn` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+    `spentOn` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `submittedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `editedAt` DATETIME(3) NULL,
     `amount` DECIMAL(10, 2) NULL,
     `description` VARCHAR(191) NULL,
     `categoryId` INTEGER NULL,
+    `claimId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -52,7 +56,16 @@ CREATE TABLE `Attachment` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
+ALTER TABLE `Claim` ADD CONSTRAINT `Claim_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `Expense` ADD CONSTRAINT `Expense_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Expense` ADD CONSTRAINT `Expense_claimId_fkey` FOREIGN KEY (`claimId`) REFERENCES `Claim`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Expense` ADD CONSTRAINT `Expense_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Attachment` ADD CONSTRAINT `Attachment_uploaderId_fkey` FOREIGN KEY (`uploaderId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
