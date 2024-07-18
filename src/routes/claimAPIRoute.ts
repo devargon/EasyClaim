302a2036901bba1a56848565cdf5ea0c3fdba1b4
Argon <argon@nogra.xyz>
2024-07-18 13:36:37 +0800

    Add delete claim endpoint

--- src/routes/claimAPIRoute.ts
+++ src/routes/claimAPIRoute.ts
@@ -2,11 +2,38 @@ import express, {Request, Response, NextFunction} from 'express';
 import {redirectAsRequiresLogin} from "../middlewares/redirectAsRequiresLogin";
 import currency from "currency.js";
 import {findExpenseByIdAndUser} from "../services/expenseService";
-import {createClaim} from "../services/claimService";
+import {createClaim, deleteClaim, findClaimByIdAndUserId} from "../services/claimService";
+import prisma from "../config/db";
 
 const router = express.Router();
 
 // noinspection JSUnusedLocalSymbols
+
+router.post("/:claimId/cancel", redirectAsRequiresLogin, async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user) {
+        return res.status(401).send();
+    }
+    const claimId = req.params.claimId;
+    const claimIdActual = parseInt(claimId, 10);
+    if (isNaN(claimIdActual)) {
+        return res.status(400).json({error_message: "not a valid claimId"});
+    }
+    const claim = await prisma.claim.findUnique({where: {id: claimIdActual, userId: req.user.id}});
+    if (!claim) {
+        return res.status(404).json({error_message: `Claim #${claimIdActual} not found.`});
+    }
+    if (claim.status === "COMPLETED") {
+        return res.status(400).json({error_message: `Claim #${claimIdActual} is complete and can't be deleted.`});
+    } else {
+        const a = await deleteClaim(claim.id);
+        if (a) {
+            return res.status(200).json({success_message: `Expense #${claimIdActual} deleted.`});
+        } else {
+            return res.status(404).json({error_message: `Expense #${claimIdActual} not found.`});
+        }
+    }
+})
+
 router.post('/new', redirectAsRequiresLogin, async (req: Request, res: Response, next: NextFunction) => {
     if (!req.user) {
         return res.status(401).send();
@@ -29,6 +56,9 @@ router.post('/new', redirectAsRequiresLogin, async (req: Request, res: Response,
         if (!expense) {
             return res.status(400).json({error_message: "At least one or more expenses could not be found while making this claim."});
         }
+        if (expense.claimId || expense.claimComplete) {
+            return res.status(400).json({error_message: "At least one or more expenses has already been included in a claim."})
+        }
         expenseTotal = expenseTotal.add(Number(expense.amount));
         foundExpenses.push(expense);
         foundExpensesIds.push(actualExpenseId);
@@ -42,7 +72,7 @@ router.post('/new', redirectAsRequiresLogin, async (req: Request, res: Response,
         return res.status(400).json({error_message: "Offset amount is more than the total amount of the expenses."});
     }
 
-    const claim = await createClaim(req.user.id, foundExpensesIds, offsetAmount);
+    const claim = await createClaim(req.user.id, foundExpensesIds, expenseTotal.value, offsetAmount);
     if (claim) {
         return res.status(200).json(claim);
     } else {
