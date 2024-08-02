1608ed38c443f22198e535a5ddfe5d46852f06cf
Argon <argon@nogra.xyz>
2024-08-02 10:14:57 +0800

    Redirect share + lang to normal share

--- src/routes/claimRoute.ts
+++ src/routes/claimRoute.ts
@@ -4,10 +4,16 @@ import {redirectAsRequiresLogin} from "../middlewares/redirectAsRequiresLogin";
 import {findAllClaimsByUserId, findClaimByShareId} from "../services/claimService";
 import currency from "currency.js";
 import {showClaims, showSharedClaim} from "../controllers/claimController";
+import {formatMoney} from "../config/utils";
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', redirectAsRequiresLogin, showClaims);
+
+router.get('/share/:lang/:shareId', async (req, res,next) => {
+    const shareId = req.params.lang || "";
+    return res.redirect(`/claims/share/${shareId}`);
+});
 
 router.get('/share/:shareId', showSharedClaim);
 
