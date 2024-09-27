2b0136e8ac634b76fdf4758b641ff35b3c63a442
Argon <argon@nogra.xyz>
2024-09-27 14:58:28 +0800

    Use 1000 instead of 1024 for file size calculation

--- src/public/assets/javascripts/main.js
+++ src/public/assets/javascripts/main.js
@@ -133,7 +133,8 @@ function formatFileSize(bytes) {
     const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
     if (bytes === 0) return '0 B';
     const i = Math.floor(Math.log(bytes) / Math.log(1024));
-    return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
+    // return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
+    return `${(bytes / Math.pow(1000, i)).toFixed(2)} ${sizes[i]}`;
 }
 
 function getAllExpenseCardElements() {
